1a54bb45b5cd18b9d0fa74293e80b57b846ce037
Boon Hui Chiann <hcboon.2017@sis.smu.edu.sg>
2019-03-25 16:05:59 +0800

    minor changes Signed-off-by: Boon Hui Chiann <hcboon.2017@sis.smu.edu.sg>

--- base/app/forms.py
+++ base/app/forms.py
@@ -8,7 +8,8 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('')
+    #submit = SubmitField('Sign In')
 
 class StudentForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
