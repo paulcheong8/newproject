3c66f1364b3eefa9934997c3bd94eccdf01e0081
root <root@DESKTOP-7AAJG9J.localdomain>
2019-03-25 17:22:50 +0800

    heroku deployment changes

--- base/config.py
+++ base/config.py
@@ -4,8 +4,9 @@ class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Chicken101@localhost/SmartAttendance'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Chicken101@localhost/SmartAttendance'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 #     POSTGRES = {
 #     'user': 'postgres',
 #     'pw': 'Chicken101',
