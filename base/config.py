3ca64cad12d93fc87c8abdf12d81b1d6346e1b1d
paulcheong8 <paul.cheong.2017@sis.smu.edu.sg>
2019-04-08 13:57:25 +0800

    attendance.html

--- base/config.py
+++ base/config.py
@@ -5,9 +5,9 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Chicken101@localhost/SmartAttendance'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     # LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    # SQLALCHEMY_DATABASE_URI = 'postgres://lgibhcwcleouzl:4e13a89c0c0b64ec0f0e0ea916456888d697630cc8fdf40af9977ecb4cd0cec2@ec2-23-23-241-119.compute-1.amazonaws.com:5432/da6um0if08b6hq'
+    SQLALCHEMY_DATABASE_URI = 'postgres://lgibhcwcleouzl:4e13a89c0c0b64ec0f0e0ea916456888d697630cc8fdf40af9977ecb4cd0cec2@ec2-23-23-241-119.compute-1.amazonaws.com:5432/da6um0if08b6hq'
 # export DATABASE_URL="postgresql://lgibhcwcleouzl:4e13a89c0c0b64ec0f0e0ea916456888d697630cc8fdf40af9977ecb4cd0cec2@ec2-23-23-241-119.compute-1.amazonaws.com:5432/da6um0if08b6hq"
 #     POSTGRES = {
 #     'user': 'postgres',
